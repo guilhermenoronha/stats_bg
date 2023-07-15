60b18944121441332c73cc2f545ef4888fc84670
Guilherme Noronha <guilhermenoronha2001@gmail.com>
2023-07-15 18:54:47 -0300

    Code refactoring and minor fixes.

--- data_wrangler.py
+++ data_wrangler.py
@@ -2,23 +2,17 @@ import pandas as pd
 import sqlite3
 from decouple import config
 
-# global variables
-_conn = sqlite3.connect('bg.db')
-_SHEET_ID = config('SHEET_ID')
+def get_url(sheet_name):
+    sheet_id = config('SHEET_ID')
+    return f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
 
-def _get_url(SHEET_NAME):
-    return f'https://docs.google.com/spreadsheets/d/{_SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
+def create_connection():
+    return sqlite3.connect('bg.db')
 
-def _get_name_id_mapping(SHEET_NAME, KEY, VALUE):
-        url = _get_url(SHEET_NAME)
-        df = pd.read_csv(url)
-        name_id_mapping = df.set_index(KEY)[VALUE].to_dict()
-        return name_id_mapping 
-
-def _create_persons_table():
+def createpersons_table(conn):
     try:
-        persons  = pd.read_csv(_get_url('persons'))
-        matches = pd.read_csv(_get_url('matches'))
+        persons  = pd.read_csv(get_url('persons'))
+        matches = pd.read_csv(get_url('matches'))
         matches.drop(columns=['game', 'host'], inplace=True)
         # mapping the person name with the last the day he/she attended to a match 
         dict_ = {}
@@ -26,72 +20,70 @@ def _create_persons_table():
             dict_.update({matches.columns[i] : matches.loc[matches.iloc[:, i].notnull(), 'date'].iloc[-1]})
         persons['LAST_DATE_ATTENDED'] = persons['NAME'].map(dict_)
         persons['DAYS_SINCE_LAST_ATTENDANCE'] = (pd.to_datetime('today') - pd.to_datetime(persons['LAST_DATE_ATTENDED'], format='%d/%m/%y')).dt.days 
-        persons.to_sql(name='PERSONS', con=_conn, if_exists='replace', index=False)
+        persons.to_sql(name='PERSONS', con=conn, if_exists='replace', index=False)
         print(f'Table PERSONS was successfully created with {len(persons)} rows.')
     except Exception as e:
-        print(f'Error: {e}')
+        print(f'Error creating PERSONS table: {e}')
 
-def _create_games_table():
+def create_games_table(conn):
     try:
-        games = pd.read_csv(_get_url('games'))
-        games['ID'] = range(1, len(games) + 1)
+        games = pd.read_csv(get_url('games'))
+        matches = pd.read_csv(get_url('matches'))
         # Create column with the date the game was last played
-        matches = pd.read_csv(_get_url('matches'))
         matches.drop_duplicates(subset=['game'], keep='last', inplace=True)
         games['LAST_DATE_PLAYED'] = games['NAME'].map(matches.set_index('game')['date'])
         # Created calculated column with the amount of days a game was last played
-        games['DAYS_SINCE_LAST_PLAYED'] = (pd.to_datetime('today') - pd.to_datetime(games['DATE_LAST_PLAYED'], format='%d/%m/%y')).dt.days 
-        games.to_sql(name='GAMES', con=_conn, if_exists='replace', index=False)
+        games['DAYS_SINCE_LAST_PLAYED'] = (pd.to_datetime('today') - pd.to_datetime(games['LAST_DATE_PLAYED'], format='%d/%m/%y')).dt.days 
+        games['ID'] = range(1, len(games) + 1)
+        games.to_sql(name='GAMES', con=conn, if_exists='replace', index=False)
         print(f'Table GAMES was successfully created with {len(games)} rows.')
     except Exception as e:
-        print(f'Error {e}')
+        print(f'Error creating GAMES table: {e}')
 
-def _create_attendances_table():
+def create_attendances_table(conn):
     try:
-        persons  = pd.read_csv(_get_url('persons'))
-        matches = pd.read_csv(_get_url('matches'))
+        persons = pd.read_csv(get_url('persons'))
+        matches = pd.read_csv(get_url('matches'))
+        matches.drop(columns=['game'], inplace=True)
         # mapping persons names to ids
-        name_id_mapping = _get_name_id_mapping('persons', 'NAME', 'ID')
+        name_id_mapping = persons.set_index('NAME')['ID'].to_dict()
         name_id_mapping.update({'host':'host', 'date':'date', 'game':'game'})
-        # game column isn't needed in the attendances_table
-        tmp_attendances = matches.drop(columns=['game'])
         # changing columns to ids
-        tmp_attendances.columns = tmp_attendances.columns.map(name_id_mapping)
+        matches.columns = matches.columns.map(name_id_mapping)
         # changing host names to ids
-        tmp_attendances['host'] = tmp_attendances['host'].map(name_id_mapping)    
+        matches['host'] = matches['host'].map(name_id_mapping)    
         # changing all names in the columns to ids
         for i in range(1, len(persons) + 1):
             # filtering to only when the person attended to a game (not null)
-            mask = tmp_attendances[i].notnull()
-            tmp_attendances.loc[mask, i] = True
+            mask = matches[i].notnull()
+            matches.loc[mask, i] = True
         # creating final table
         attendances = pd.DataFrame(columns = ['DATE', 'IS_HOST', 'PERSON_ID'])
         # looping over persons
         for i in range(1, len(persons) + 1):
             person = pd.DataFrame(columns = ['DATE', 'IS_HOST', 'PERSON_ID'])
-            person['DATE'] = tmp_attendances[tmp_attendances[i] == True]['date']
+            person['DATE'] = matches[matches[i] == True]['date']
             person['PERSON_ID'] = i
-            person['IS_HOST'] = tmp_attendances['host'] == i
+            person['IS_HOST'] = matches['host'] == i
             # keeping only the first record each person attended for each date.
             person = person.drop_duplicates(subset=['DATE', 'PERSON_ID'], keep='first')
             attendances = pd.concat([attendances, person])
-        attendances.to_sql(name='ATTENDANCES', con=_conn, if_exists='replace', index=False)
+        attendances.to_sql(name='ATTENDANCES', con=conn, if_exists='replace', index=False)
         print(f'Table ATTENDANCES was successfully created with {len(attendances)} rows.')
     except Exception as e:
-        print(f'Error: {e}')
+        print(f'Error creating ATTENDANCES table: {e}')
 
-def _create_matches_table():
+def create_matches_table(conn):
     try:
-        matches = pd.read_csv(_get_url('matches'))
-        persons = pd.read_csv(_get_url('persons'))
-        games = pd.read_csv(_get_url('games'))
+        games = pd.read_csv(get_url('games'))
         games['ID'] = range(1, len(games) + 1)
-        final_matches = pd.DataFrame(columns = ['DATE', 'ID', 'PERSON_ID', 'GAME_ID', 'SCORE'])
+        persons = pd.read_csv(get_url('persons'))
+        matches = pd.read_csv(get_url('matches'))
         # host column isn't needed in matches table 
         matches.drop(columns=['host'], inplace=True)
         # mapping persons names to ids
-        name_id_mapping = _get_name_id_mapping('persons', 'NAME', 'ID')
-        name_id_mapping.update({'host':'host', 'date':'date', 'game':'game'})  
+        name_id_mapping = persons.set_index('NAME')['ID'].to_dict()
+        name_id_mapping.update({'host':'host', 'date':'date', 'game':'game'})
         matches.columns = matches.columns.map(name_id_mapping)
         # creating ID column
         matches['ID'] = range(1, len(matches) + 1)
@@ -99,6 +91,7 @@ def _create_matches_table():
         game_id_mapping = games.set_index('NAME')['ID'].to_dict()
         matches['game'] = matches['game'].map(game_id_mapping)
         # looping over persons
+        final_matches = pd.DataFrame(columns = ['DATE', 'ID', 'PERSON_ID', 'GAME_ID', 'SCORE'])
         for i in range(1, len(persons) + 1):
             tmp_matches = matches[matches[i].notnull()]
             person_matches = pd.DataFrame(columns = ['DATE', 'ID', 'PERSON_ID', 'GAME_ID', 'SCORE'])
@@ -108,16 +101,14 @@ def _create_matches_table():
             person_matches['GAME_ID'] = tmp_matches['game']
             person_matches['SCORE'] = tmp_matches[i]
             final_matches = pd.concat([final_matches, person_matches])
-        final_matches.to_sql(name='MATCHES', con=_conn, if_exists='replace', index=False)
+        final_matches.to_sql(name='MATCHES', con=conn, if_exists='replace', index=False)
         print(f'Table MATCHES was successfully created with {len(final_matches)} rows.')
     except Exception as e:
-        print(f'Error: {e}')
-
+        print(f'Error creating MATCHES table: {e}')
 
 if __name__ == '__main__':
-    _create_persons_table()
-    _create_games_table()
-    _create_attendances_table()
-    _create_matches_table()
-    
-
+    conn = sqlite3.connect('bg.db')
+    createpersons_table(conn)
+    create_games_table(conn)
+    create_attendances_table(conn)
+    create_matches_table(conn)
