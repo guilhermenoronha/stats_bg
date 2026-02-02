c282be093e8ed064e20c06696985421fde189c27
Guilherme Noronha <guilhermenoronha2001@gmail.com>
2026-02-02 20:34:53 -0300

    Added sommelier of the year KPI. close #46

--- dbt/models/gold/game_owners_per_match.sql
+++ dbt/models/gold/game_owners_per_match.sql
@@ -6,14 +6,21 @@ matches as (select * from {{ ref('matches') }}),
 games as (select * from {{ ref('games') }}),
 
 final as (
-    select 
+    select distinct
         p."name", 
-        m."id" as match_id, 
-        g.name as game_name
+        m."id" as match_id,
+        m."date",
+        g.name as game_name,
+        dense_rank() over(partition by game_name, EXTRACT(YEAR FROM date) order by date, m."id") as dr
     from players p
     left join bg_owners bo on bo."user_id" = p."id"
     join matches m on m."game_id" = bo."game_id"
     left join games g on g."id"  = bo."game_id"
 )
 
-select * from final
+select 
+  name, 
+  match_id,
+  game_name, 
+  1/cast(dr as float) as sommelier_rank 
+from final
